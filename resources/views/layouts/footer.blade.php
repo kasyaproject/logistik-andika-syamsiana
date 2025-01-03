f9c218c0eb41d13d35976190582be21b7d25a41f
sikadep <andikayamana14045@gmail.com>
2025-01-03 23:29:48 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="z-50 w-full py-8 text-sm text-center text-black bg-black dark:text-white/70">
+    <span class="font-semibold text-white">by : Andika Syamsiana</span> | <span class="text-gray-400">Laravel
+        v{{ Illuminate\Foundation\Application::VERSION }}
+        (PHP
+        v{{ PHP_VERSION }})</span>
+</footer>
